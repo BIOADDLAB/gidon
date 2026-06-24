0cdebdbb1adb497653c20f4ac481481a4805cb06
유경아 <ukyunga0.1@gmail.com>
2026-06-24 15:30:41 +0900

    feat: 타입추가 및 마진 위치 변경 bottom -> top

--- src/components/common/SectionHeading.tsx
+++ src/components/common/SectionHeading.tsx
@@ -2,19 +2,20 @@ interface SectionHeadingProps {
     topTitle: string;
     centerTitle: string;
     desc: string;
+    isDesc?: boolean;
 }
 
-export default function SectionHeading({ topTitle, centerTitle, desc }: SectionHeadingProps) {
+export default function SectionHeading({ topTitle, centerTitle, desc, isDesc }: SectionHeadingProps) {
     return (
         <div className="text-center">
             {/* 작은 상단 카테고리/텍스트 */}
-            <p className={`font-accent text-[#213225] mb-2`}>{topTitle}</p>
+            <p className="font-accent text-[#213225]">{topTitle}</p>
 
             {/* 섹션의 메인 큰 제목 (SEO 핵심) */}
-            <h2 className="text-[40px] font-bold text-[#213225] mb-[24px]">{centerTitle}</h2>
+            <h2 className="text-[40px] font-bold text-[#213225] mt-8">{centerTitle}</h2>
 
             {/* 하단 설명글 */}
-            <h3 className="text-[20px] text-[#19251e] font-normal">{desc}</h3>
+            {isDesc ? <h3 className="text-[20px] text-[#19251e] mt-[24px] font-normal">{desc}</h3> : null}
         </div>
     );
 }
