4a27f7c61bee2d646a311e4efebbff01d38e797d
유경아 <ukyunga0.1@gmail.com>
2026-06-24 15:34:59 +0900

    style: 마진 값 변경

--- src/components/common/SectionHeading.tsx
+++ src/components/common/SectionHeading.tsx
@@ -12,7 +12,7 @@ export default function SectionHeading({ topTitle, centerTitle, desc, isDesc }: 
             <p className="font-accent text-[#213225]">{topTitle}</p>
 
             {/* 섹션의 메인 큰 제목 (SEO 핵심) */}
-            <h2 className="text-[40px] font-bold text-[#213225] mt-8">{centerTitle}</h2>
+            <h2 className="text-[40px] font-bold text-[#213225] mt-1">{centerTitle}</h2>
 
             {/* 하단 설명글 */}
             {isDesc ? <h3 className="text-[20px] text-[#19251e] mt-[24px] font-normal">{desc}</h3> : null}
