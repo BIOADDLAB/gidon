ea50a2f5fffd7e50f858c74cbcd4dbaf1458e5b1
유경아 <ukyunga0.1@gmail.com>
2026-06-29 10:41:56 +0900

    wip: 오시는길 페이지 작업중

--- src/app/location/page.tsx
+++ src/app/location/page.tsx
@@ -1,17 +1,228 @@
 import HeroSection from '@/components/HeroSection';
+import SectionHeading from './../../components/common/SectionHeading';
+import Image from 'next/image';
 
 export default function LocationPage() {
     const locationMenuList = ['오시는길'];
     return (
-        <HeroSection
-            mainTitle="오시는길"
-            subTitle="Location"
-            pageName="오시는길"
-            subNavItem="오시는길"
-            imgSrc="/images/bg_reco.jpg"
-            isNav={true}
-            isGreen={false}
-            subMenuList={locationMenuList}
-        />
+        <main>
+            <HeroSection
+                mainTitle="오시는길"
+                subTitle="Location"
+                pageName="오시는길"
+                subNavItem="오시는길"
+                imgSrc="/images/bg_green2.jpg"
+                isNav={true}
+                isGreen={false}
+                subMenuList={locationMenuList}
+            />
+
+            <section className="bg-[#F9FAF5] pt-[100px] pb-[124px]">
+                <div className="w-[930px] mx-auto  ">
+                    <SectionHeading
+                        topTitle="GIDEON’S Location"
+                        centerTitle="기드온치과 오시는 길"
+                        desc="기드온치과로 오시는 안전하고 편안한 길을 안내해 드립니다."
+                        isDesc={true}
+                    />
+
+                    <div className="flex justify-center items-stretch gap-13 mt-[60px]">
+                        {/* 왼쪽 영역 */}
+                        <div className="w-[452px] relative">
+                            <Image
+                                src="/images/bg_green2.jpg"
+                                sizes="452px"
+                                fill
+                                className="object-cover rounded-[10px]"
+                                alt="건물 외관 전경"
+                            />
+                        </div>
+
+                        {/* 정보 영역 */}
+                        <div className="flex-1 border border-green-600/40 rounded-[10px] flex flex-col justify-between">
+                            {/* 주소 */}
+                            <div className="p-[45px] flex-1">
+                                <div className="flex items-center gap-2  pb-2 mb-4">
+                                    <span className="w-[33px] h-[33px] bg-green-600 rounded-full flex items-center justify-center">
+                                        <img
+                                            src="/images/i_loca_m.svg"
+                                            alt="주소 아이콘"
+                                            className="w-5 h-5 object-contain"
+                                        />
+                                    </span>
+                                    <h3 className="text-[25px] text-green-600 font-semibold tracking-[-0.03em] leading-[1.5]">
+                                        주소
+                                    </h3>
+                                </div>
+                                <p className="text-[18px] text-[#3d3e3c] font-medium leading-relaxed">
+                                    경기도 성남시 중원구 성남대로 1126, 메가프라자 3층 <br />
+                                    <span className="font-normal text-[16px] text-gray-500 block mt-1">
+                                        모란역 4번출구 방향 도보 2분 국민은행 건물 3층
+                                    </span>
+                                </p>
+                            </div>
+
+                            {/* 전화 */}
+                            <div className="p-[45px] flex-1 border-t border-green-600/40">
+                                <div className="flex items-center gap-2  pb-2 mb-4">
+                                    <span className="w-[33px] h-[33px] bg-green-600 rounded-full flex items-center justify-center">
+                                        <img
+                                            src="/images/i_loca_p.svg"
+                                            alt="전화 아이콘"
+                                            className="w-5 h-5 object-contain"
+                                        />
+                                    </span>
+                                    <h3 className="text-[25px] text-green-600 font-semibold tracking-[-0.03em] leading-[1.5]">
+                                        전화번호
+                                    </h3>
+                                </div>
+                                <p className="text-[24px] text-[#3d3e3c] font-bold tracking-wide">
+                                    031-XXXX-XXXX
+                                    {/* #ISSUE: 전화 연결 필요 */}
+                                    <a
+                                        href="tel: 010-1111-"
+                                        className="inline-block text-[20px] font-bold py-[9px] px-[25px] rounded-[33px] mt-[13px] border border-green-600 bg-[#F4F1EA]"
+                                    >
+                                        전화 걸기
+                                    </a>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <section className="relative w-ful pt-[100px] pb-[130px]">
+                <div className="w-[1140px] mx-auto">
+                    <Image
+                        src="/images/bg_location.jpg"
+                        alt="배경 이미지"
+                        fill
+                        sizes="100vw"
+                        className="object-cover object-center -z-10"
+                    />
+                    <SectionHeading topTitle="MAP" centerTitle="지도에서 바로 확인하기" isWhite={true} />
+
+                    {/* 구글지도영역 */}
+                    <div className="w-full h-[456px] bg-[#D9D9D9] rounded-[10px] mt-[58px]"></div>
+                </div>
+            </section>
+
+            <section className="relative w-ful pt-[100px] pb-[130px]">
+                <div className="w-[1140px] mx-auto">
+                    <Image
+                        src="/images/bg_info.jpg"
+                        alt="배경 이미지"
+                        fill
+                        sizes="100vw"
+                        className="object-cover object-center -z-10"
+                    />
+
+                    <SectionHeading
+                        topTitle="TRAFFIC INFO"
+                        centerTitle="방문 시 참고사항"
+                        desc="교통편과 진료시간, 주차 정보를 상세히 확인하실 수 있습니다."
+                        isDesc={true}
+                    />
+                    {/* 안내 */}
+                    <div className="mt-[78px] flex justify-center flex-wrap gap-[20px] ">
+                        {/* 1. 지하철 이용 */}
+                        {/* 💡 최상위 부모에 bg-white, rounded, shadow, h-[350px]를 다 몰아주고 flex flex-col로 배치합니다 */}
+                        <div className="w-[calc((100%-20px)/2)] h-[350px] bg-white rounded-[10px] overflow-hidden shadow-[3px_4px_14.6px_0px_rgba(72,72,72,0.1)] flex flex-col">
+                            {/* 헤더 영역 */}
+                            <div className="py-[20px] px-[48px] bg-green-600 flex items-center gap-[16px] shrink-0">
+                                <img src="/images/i_info_1.svg" className="w-[38px] h-[42px]" alt="아이콘" />
+                                <h3 className="font-ui text-[26px] font-bold text-white">지하철 이용</h3>
+                            </div>
+                            {/* 본문 영역 - bg-white와 rounded를 제거하고 flex-1로 남은 공간을 꽉 채웁니다 */}
+                            <div className="pt-[44px] pb-[32px] px-[50px] flex-1">
+                                <div className="pb-[16px] border-b border-[#949494] mb-[26px] flex items-center gap-[14px]">
+                                    <div className="w-[30px] h-[30px] rounded-full flex justify-center items-center bg-green-600 shrink-0">
+                                        <span className="text-[20px] font-extrabold text-white">1</span>
+                                    </div>
+                                    <p className="text-[20px] font-semibold text-[#302e2e]">
+                                        모란역 4번 출구에서 직진 후 도보 2분
+                                    </p>
+                                </div>
+                                <div className="pb-[16px] border-b border-[#949494] flex gap-[14px]">
+                                    <div className="w-[30px] h-[30px] rounded-full flex justify-center items-center bg-green-600 shrink-0">
+                                        <span className="text-[20px] font-extrabold text-white">2</span>
+                                    </div>
+                                    <p className="text-[20px] font-semibold text-[#302e2e]">
+                                        국민은행이 있는 메가프라자 3층에서 <br />
+                                        기드온치과를 찾으실 수 있습니다.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+
+                        {/* 2. 버스 이용 */}
+                        <div className="w-[calc((100%-20px)/2)] h-[350px] bg-white rounded-[10px] overflow-hidden shadow-[3px_4px_14.6px_0px_rgba(72,72,72,0.1)] flex flex-col">
+                            <div className="py-[20px] px-[48px] bg-green-600 flex items-center gap-[16px] shrink-0">
+                                <img src="/images/i_info_2.svg" className="w-[38px] h-[42px]" alt="아이콘" />
+                                <h3 className="font-ui text-[26px] font-bold text-white">버스 이용</h3>
+                            </div>
+                            <div className="pt-[44px] pb-[32px] px-[50px] flex-1">
+                                <div className="pb-[16px] border-b border-[#949494] mb-[26px] flex items-center gap-[14px]">
+                                    <p className="text-[20px] font-semibold text-[#302e2e]">
+                                        모란역 4번 출구 정류장 하차 후 <br />
+                                        도보 이동이 가능합니다.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+
+                        {/* 3. 진료 시간 안내 */}
+                        <div className="w-[calc((100%-20px)/2)] h-[350px] bg-white rounded-[10px] overflow-hidden shadow-[3px_4px_14.6px_0px_rgba(72,72,72,0.1)] flex flex-col">
+                            <div className="py-[20px] px-[48px] bg-green-600 flex items-center gap-[16px] shrink-0">
+                                <img src="/images/i_info_3.svg" className="w-[38px] h-[42px]" alt="아이콘" />
+                                <h3 className="font-ui text-[26px] font-bold text-white">진료 시간 안내</h3>
+                            </div>
+                            {/* 진료 시간 카드는 항목이 많아 패딩을 살짝 여유롭게 잡았습니다 */}
+                            <div className="pt-[44px] pb-[32px] px-[50px] flex-1">
+                                <div className="pb-[12px] flex items-center justify-between">
+                                    <h4 className="text-[22px] font-semibold text-[#302e2e]">월 / 화 / 목</h4>
+                                    <p className="text-[20px] font-semibold text-[#646464]">10:00 ~ 19:00</p>
+                                </div>
+                                <div className="pb-[12px] flex items-center justify-between">
+                                    <h4 className="text-[22px] font-semibold text-[#302e2e]">금</h4>
+                                    <p className="text-[20px] font-semibold text-[#646464]">10:00 ~ 20:00</p>
+                                </div>
+                                <div className="pb-[12px] flex items-center justify-between">
+                                    <h4 className="text-[22px] font-semibold text-[#302e2e]">수 / 토 </h4>
+                                    <p className="text-[20px] font-semibold text-[#646464]">10:00 ~ 16:00</p>
+                                </div>
+                                <div className="pb-[12px] flex items-center justify-between border-b border-[#949494]">
+                                    <h4 className="text-[22px] font-semibold text-[#302e2e]">일 / 공휴일</h4>
+                                    <p className="text-[20px] font-semibold text-[#AE4F4F]">휴진</p>
+                                </div>
+                            </div>
+                        </div>
+
+                        {/* 4. 주차 안내 */}
+                        <div className="w-[calc((100%-20px)/2)] h-[350px] bg-white rounded-[10px] overflow-hidden shadow-[3px_4px_14.6px_0px_rgba(72,72,72,0.1)] flex flex-col">
+                            <div className="py-[20px] px-[48px] bg-green-600 flex items-center gap-[16px] shrink-0">
+                                <img src="/images/i_info_4.svg" className="w-[38px] h-[42px]" alt="아이콘" />
+                                <h3 className="font-ui text-[26px] font-bold text-white">주차 안내</h3>
+                            </div>
+                            <div className="pt-[44px] pb-[32px] px-[50px] flex-1">
+                                <div className="pb-[16px] border-b border-[#949494] mb-[26px] flex items-center gap-[14px]">
+                                    <p className="text-[20px] font-semibold text-[#302e2e]">
+                                        건물 뒷편, 기계식 카리프트 <br />
+                                        이용하시면 됩니다.
+                                    </p>
+                                </div>
+                                <div className="pb-[16px] border-b border-[#949494] flex gap-[14px]">
+                                    <p className="text-[20px] font-semibold text-[#302e2e]">
+                                        접수 시 차량번호 말씀해 주시면 <br />
+                                        필요한 안내를 도와드립니다.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </main>
     );
 }
--- src/components/common/SectionHeading.tsx
+++ src/components/common/SectionHeading.tsx
@@ -3,19 +3,29 @@ interface SectionHeadingProps {
     centerTitle: string;
     desc?: string;
     isDesc?: boolean;
+    isWhite?: boolean;
+    isLh?: boolean;
 }
 
-export default function SectionHeading({ topTitle, centerTitle, desc, isDesc }: SectionHeadingProps) {
+export default function SectionHeading({ topTitle, centerTitle, desc, isDesc, isWhite, isLh }: SectionHeadingProps) {
     return (
         <div className="text-center">
             {/* 작은 상단 카테고리/텍스트 */}
-            <p className="font-accent text-[#213225]">{topTitle}</p>
+            <p className={`font-accent ${isWhite ? 'text-[#fff]' : ' text-[#213225]'}`}>{topTitle}</p>
 
             {/* 섹션의 메인 큰 제목 (SEO 핵심) */}
-            <h2 className="text-[40px] font-bold text-[#213225] mt-1">{centerTitle}</h2>
+            <h2 className={`text-[40px] font-bold ${isWhite ? 'text-[#fff]' : ' text-[#213225]'} mt-1`}>
+                {centerTitle}
+            </h2>
 
             {/* 하단 설명글 */}
-            {isDesc ? <h3 className="text-[20px] text-[#19251e] mt-[24px] font-normal">{desc}</h3> : null}
+            {isDesc ? (
+                <h3
+                    className={`text-[20px] text-[#19251e] whitespace-pre-wrap mt-6 font-normal ${isLh ? 'leading-none' : ''}`}
+                >
+                    {desc}
+                </h3>
+            ) : null}
         </div>
     );
 }
